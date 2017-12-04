ed3491c91a2e4bd51532f67a1700af22e935d290
hoangtuyb96 <hoangtuyb96@gmail.com>
2017-12-04 10:33:08 +0700

    add_my_page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,6 +8,7 @@
           <li><a href="#">Contact</a></li>
           @if(Auth::check())
             <li><a href="/{{Auth::user()->username}}/profile">{{Auth::user()->fullname}}</a></li>
+            <li><a href="/{{Auth::user()->username}}/my_page">My Page</a></li>
             <li><a href="/{{'logout'}}">Logout</a></li>
             <!-- <li class="has-dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Auth::user()->fullname}} <span class="caret"></span></a>
