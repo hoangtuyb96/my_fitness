a0db26b18b0adc647d2e5b8915aa01ba3a6a63ee
thanhthudhbkhn <thanhthuk59@gmail.com>
2017-12-05 22:24:00 +0700

    add_paginate_programs

--- app/Http/Controllers/ProgramsController.php
+++ app/Http/Controllers/ProgramsController.php
@@ -8,7 +8,7 @@ use App\Program;
 class ProgramsController extends Controller
 {
     function index(){
-      $programs = Program::simplePaginate(5);
+      $programs = Program::paginate(5);
       return view('programs.index')->with('programs', $programs);
     }
 
