9d7cd8da7f2ce0e47a28281015d31acd85adce3b
lampv <lampvhy@gmail.com>
2017-12-06 17:55:27 +0700

    check some convention

--- app/Http/Controllers/ProgramsController.php
+++ app/Http/Controllers/ProgramsController.php
@@ -7,19 +7,19 @@ use App\Program;
 
 class ProgramsController extends Controller
 {
-    function index(){
-      $programs = Program::paginate(6);
-      return view('programs.index')->with('programs', $programs);
-    }
+  function index(){
+    $programs = Program::paginate(6);
+    return view('programs.index')->with('programs', $programs);
+  }
 
-    function show($id){
-      $program = Program::find($id);
-      $pcheck = Program::find($id);
-      $user = $program->user;
-      return view('programs.show')->with([
-        'program' => $program,
-        'user' => $user,
-        'pcheck' => $pcheck
-      ]);
-    }
+  function show($id){
+    $program = Program::find($id);
+    $pcheck = Program::find($id);
+    $user = $program->user;
+    return view('programs.show')->with([
+      'program' => $program,
+      'user' => $user,
+      'pcheck' => $pcheck
+    ]);
+  }
 }
--- app/MyProgram.php
+++ app/MyProgram.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class MyProgram extends Model
 {
-    protected $table = "my_programs";
+  protected $table = 'my_programs';
 
-    public function user(){
-      return $this->belongsTo('App\User', 'user_id', 'id');
-    }
+  public function user(){
+    return $this->belongsTo('App\User', 'user_id', 'id');
+  }
 
-    public function program(){
-      return $this->belongsTo('App\Program', 'program_id', 'id');
-    }
+  public function program(){
+    return $this->belongsTo('App\Program', 'program_id', 'id');
+  }
 }
