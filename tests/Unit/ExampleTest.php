fe189c979ec62691348d3c53286bdc38692778fa
lampv <lampvhy@gmail.com>
2017-12-14 15:39:27 +0700

    unit_test_sign_up_login

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -16,4 +16,33 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testLogin()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+        $response->assertSee('Login');
+        $res = $this->json('POST', '/login', ['username' => 'hoangtuyb96', 'password' => '863991']);
+        $res->assertStatus(302);
+        $res->assertRedirect('hoangtuyb96/my_page');
+        $this->followRedirects($res)->assertSee('Profile');
+    }
+
+    public function testSignup()
+    {
+        $response = $this->get('/signup');
+        $response->assertStatus(200);
+        $response->assertSee('Signup');
+        $res = $this->json('POST', '/signup', [
+            'username' => 'test2',
+            'email'    => 'test2@gmail.com',
+            'password' => '863991',
+            'password_confirmation' => '863991',
+            'fullname' => 'test',
+            'purpose'  => 1
+            ]);
+        $res->assertStatus(302);
+        $res->assertRedirect('/');
+        $this->followRedirects($res)->assertSee('Home');
+    }
 }
