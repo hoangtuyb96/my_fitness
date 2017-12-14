25215d8d9eb8530d70e88b54469e535c5de28760
GIOsBK <hatran10091996@gmail.com>
2017-12-14 16:19:16 +0700

    edit_interface

--- resources/views/users/signup.blade.php
+++ resources/views/users/signup.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="div-1">
-<div class="container">
+<div class="full-container">
 
   <div class="row">
       <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
